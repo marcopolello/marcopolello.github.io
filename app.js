3182d9784e734e72970372d95486609f90c2ba03
marcopolello <marco-pol@hotmail.it>
2021-02-17 21:01:29 +0100

    animations home

--- app.js
+++ app.js
@@ -7,7 +7,83 @@
   \********************/
 /***/ (() => {
 
+//select DOM Items
+var menuBtn = document.querySelector(".menu-btn");
+var menu = document.querySelector(".menu");
+var menuNav = document.querySelector(".menu-nav");
+var menuBranding = document.querySelector(".menu-branding");
+var navItems = document.querySelectorAll(".nav-item"); // Set Initial state of Menu
 
+var showMenu = false;
+menuBtn.addEventListener("click", toggleMenu);
+
+function toggleMenu() {
+  if (!showMenu) {
+    menuBtn.classList.add("close");
+    menu.classList.add("show");
+    menuNav.classList.add("show");
+    menuBranding.classList.add("show");
+    navItems.forEach(function (item) {
+      return item.classList.add("show");
+    });
+    showMenu = true;
+  } else {
+    menuBtn.classList.remove("close");
+    menu.classList.remove("show");
+    menuNav.classList.remove("show");
+    menuBranding.classList.remove("show");
+    navItems.forEach(function (item) {
+      return item.classList.remove("show");
+    });
+    showMenu = false;
+  }
+}
+
+var colors = {
+  primaryColor: "black",
+  secondaryColor: "#25b0e7"
+}; // chart js
+
+var ctx = document.getElementById("myChart").getContext("2d");
+var chart = new Chart(ctx, {
+  // The type of chart we want to create
+  type: "horizontalBar",
+  // The data for our dataset
+  data: {
+    labels: ["HTML", "CSS", "Javascript", "JQuery", "MySQL", "Php", "Laravel", "VueJs"],
+    datasets: [{
+      label: "Conoscenza",
+      backgroundColor: colors.secondaryColor,
+      borderColor: "green",
+      borderWidth: 1,
+      data: [80, 80, 60, 60, 50, 65, 62, 80]
+    }]
+  },
+  // Configuration options go here
+  options: {
+    legend: {
+      display: false,
+      labels: {
+        // This more specific font property overrides the global property
+        fontColor: "white"
+      }
+    },
+    scales: {
+      yAxes: [{
+        ticks: {
+          // beginAtZero: true,
+          fontColor: "white"
+        }
+      }],
+      xAxes: [{
+        ticks: {
+          beginAtZero: true,
+          fontColor: "white"
+        }
+      }]
+    }
+  }
+});
 
 /***/ }),
 
